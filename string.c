fb1238436b2c08f26978d68a52905f89bc645d38
Aditya <addhiman245@gmail.com>
2025-11-05 11:09:18 +0530

    VERSION 5

--- string.c
+++ string.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(){
-    
+// incomplete
+
+
+int main() {
     int a = 1;
-    char name[] = "";
-    char * ptr = &name;
-    ptr = (char*) malloc(1 * sizeof(char)); // Initially allocate for a char
+    char name[100];      // buffer for input
+    char *ptr = NULL;    // dynamic pointer
 
-    do{
-        fgets(name,sizeof(name),stdin);
-        name[strlen(name) - 1 ] = '\0';
-        ptr = (char*) realloc(ptr, a * sizeof(char)); // Reallocate to store a char
-        if (ptr == NULL) {
-        exit(0);
-        
-    }
-    a++;
+    ptr = (char*) malloc(1 * sizeof(char));
 
-    }
-    while(name[strlen(name) - 1 ] != '\n');
+    do {
+        fgets(name, sizeof(name), stdin);
 
-return 0;
+        if(name[strlen(name)-1] == '\n')
+            name[strlen(name)-1] = '\0';
+
+        ptr = (char*) realloc(ptr, a * sizeof(char));
+        if(ptr == NULL) {
+            exit(0);
+        }
+        a++;
+
+    } while(name[strlen(name) - 1] != '\0'); // end when newline removed
+
+    printf("%s",name);
+    return 0;
 }
